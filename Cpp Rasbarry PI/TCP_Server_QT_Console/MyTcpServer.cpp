698065238f7f3ae37b195395ce0479f715439832
krnie20 <71767500+krnie20@users.noreply.github.com>
2021-03-17 11:41:00 +0100

    [Working] Tcp server can send and receive

--- Cpp Rasbarry PI/TCP_Server_QT_Console/MyTcpServer.cpp
+++ Cpp Rasbarry PI/TCP_Server_QT_Console/MyTcpServer.cpp
@@ -1,4 +1,5 @@
 #include "MyTcpServer.h"
+#include <iostream>
 
 MyTcpServer::MyTcpServer(QObject *parent) :
     QObject(parent)
@@ -20,13 +21,27 @@ MyTcpServer::MyTcpServer(QObject *parent) :
 }
 
 void MyTcpServer::newConnection(){
+    char buffer[256]{};
+
     // need to grab the socket
     QTcpSocket *socket = server->nextPendingConnection();
 
     socket->write("ER I SNART FAERDIGE!?\r\n");
     socket->flush();
-
     socket->waitForBytesWritten(3000);
+    socket->waitForReadyRead(3000);
+
+    socket->read(buffer, 255);
+
+    for (size_t i = 0; i < 256 ; i++) {
+        if (buffer[i] != NULL){
+            std::cout << buffer[i];
+        }
+    }
+
+    if (buffer[0] != NULL){
+        std::cout << std::endl;
+    }
 
     socket->close();
 }
