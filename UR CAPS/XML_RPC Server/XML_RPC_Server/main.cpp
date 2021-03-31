3a577dbe106996fa1851d62a45869f3487b97bfc
J4RB <jlarsen2107@gmail.com>
2021-03-31 05:28:57 -0700

    Alot of shit

---
+++ UR CAPS/XML_RPC Server/XML_RPC_Server/main.cpp
@@ -0,0 +1,131 @@
+/*
+#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <stdexcept>
+#include <cassert>
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <xmlrpc-c/base.hpp>
+#include <xmlrpc-c/registry.hpp>
+#include <xmlrpc-c/server_abyss.hpp>
+
+class test : public xmlrpc_c::method {
+public:
+    test() {
+        this->_signature = "i:";
+        this->_help = "Connection check function";
+    }
+    void
+    execute(xmlrpc_c::paramList const& paramList, xmlrpc_c::value* const retvalP) {
+        std::cout << "Hello there!" << std::endl;
+
+        *retvalP = xmlrpc_c::value_int(42);
+    }
+};
+
+int main()
+{
+    std::cout << "Starting XML_RPC Server" << std::endl;
+    try {
+        xmlrpc_c::registry myRegistry;
+
+        xmlrpc_c::methodPtr const areYouThere(new test);
+
+        myRegistry.addMethod("are_you_there", areYouThere);
+        xmlrpc_c::serverAbyss myAbyssServer(xmlrpc_c::serverAbyss::constrOpt().registryP(&myRegistry).portNumber(56677));
+
+        myAbyssServer.run();
+
+        assert(false);
+
+    }  catch (std::exception const& e) {
+        std::cerr << "Something failed. " << e.what() << std::endl;
+    }
+
+    return 0;
+}
+*/
+
+#define WIN32_LEAN_AND_MEAN  /* required by xmlrpc-c/server_abyss.hpp */
+
+#include <cassert>
+#include <stdexcept>
+#include <iostream>
+#ifdef _WIN32
+#  include <windows.h>
+#else
+#  include <unistd.h>
+#endif
+
+using namespace std;
+
+#include <xmlrpc-c/base.hpp>
+#include <xmlrpc-c/registry.hpp>
+#include <xmlrpc-c/server_abyss.hpp>
+
+#ifdef _WIN32
+  #define SLEEP(seconds) SleepEx(seconds * 1000);
+#else
+  #define SLEEP(seconds) sleep(seconds);
+#endif
+
+
+class sampleAddMethod : public xmlrpc_c::method {
+public:
+    sampleAddMethod() {
+        // signature and help strings are documentation -- the client
+        // can query this information with a system.methodSignature and
+        // system.methodHelp RPC.
+        this->_signature = "i:ii";
+            // method's result and two arguments are integers
+        this->_help = "This method adds two integers together";
+    }
+    void
+    execute(xmlrpc_c::paramList const& paramList,
+            xmlrpc_c::value *   const  retvalP) {
+
+        int const addend(paramList.getInt(0));
+        int const adder(paramList.getInt(1));
+
+        paramList.verifyEnd(2);
+
+        *retvalP = xmlrpc_c::value_int(addend + adder);
+
+        // Sometimes, make it look hard (so client can see what it's like
+        // to do an RPC that takes a while).
+        if (adder == 1)
+            SLEEP(2);
+    }
+};
+
+
+
+int
+main(int           const,
+     const char ** const) {
+
+    try {
+        xmlrpc_c::registry myRegistry;
+
+        xmlrpc_c::methodPtr const sampleAddMethodP(new sampleAddMethod);
+
+        myRegistry.addMethod("sample.add", sampleAddMethodP);
+
+        xmlrpc_c::serverAbyss myAbyssServer(
+            xmlrpc_c::serverAbyss::constrOpt()
+            .registryP(&myRegistry)
+            .portNumber(8080));
+
+        myAbyssServer.run();
+        // xmlrpc_c::serverAbyss.run() never returns
+        assert(false);
+    } catch (exception const& e) {
+        cerr << "Something failed.  " << e.what() << endl;
+    }
+    return 0;
+}
